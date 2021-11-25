bac6db0c24da9286218f35fd56c377cf7a3d851d
Akash Randhawa <akashrandhawa@hotmail.com>
2021-11-24 16:04:08 -0800

    Added solution

---
+++ Amazoom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amazoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
